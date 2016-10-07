8fb76f9a8bed1c7debf7ad3a4ec0be6458970298
guille777 <gbrfloridaa@gmail.com>
2016-10-07 10:18:10 +0200

    Fase 1

---
+++ lib/ia.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Nave espacial que se mueve
+ * Guillermo
+ */
+class ia{
+
+public $fila = "1";
+public $columna = "2";
+
+	public function getFila(){
+	return $this->fila;
+	}
+
+	public function getColumna(){
+	return $this->columna;
+	}
+}
+?>
